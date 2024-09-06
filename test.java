3c103f47d7474dacd855d053cd57e940d61deef1
wmz <1303016331@qq.com>
2024-09-06 13:46:34 +0800

    haloo

---
+++ test.java
@@ -0,0 +1,13 @@
+/**
+*@BelongsProject: java_test
+*@BelongsPackage: PACKAGE_NAME
+*@Author: yanhongwei
+*@CreateTime: 2024-09-06  13:45
+*@Description: TODO
+*@Version: 1.0
+*/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hallo world!");
+    }
+}
